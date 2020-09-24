4820c8d7128dbe39f85ef538b20eca2d311a66b7
hugues <hugueslaborde69@gmail.com>
2020-09-24 23:07:50 +0200

    Explosion Fx
    Added explosion fx when the bullet enter a trigger

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,12 +6,29 @@ public class Bullet : MonoBehaviour
 {
 
     Cinemachine.CinemachineImpulseSource screenShake;
+    [SerializeField] private GameObject hitEffect;
 
     // Start is called before the first frame update
     void Start()
     {
         screenShake = GetComponent<Cinemachine.CinemachineImpulseSource>();
         screenShake.GenerateImpulse();
+    }
+
+    /*
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity); //Quaternion.identity = no rotation
+        Destroy(effect, 0.85f);
+        Destroy(gameObject);
+    }
+    */
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity); //Quaternion.identity = no rotation
+        Destroy(effect, 0.85f);
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
